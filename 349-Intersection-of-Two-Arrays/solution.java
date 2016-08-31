83f2b5e7788849d6156ca133f93a533586bf209b
qiuyu6 <qiuyu_93@hotmail.com>
2016-08-30 21:55:15 -0400

    [349. Intersection of Two Arrays][Accepted]committed by Qiu

--- 349-Intersection-of-Two-Arrays/solution.java
+++ 349-Intersection-of-Two-Arrays/solution.java
@@ -9,25 +9,20 @@ public class Solution {
         
         while((p < nums1.length) && (q < nums2.length)){
             if(nums1[p] == nums2[q]){
-                //if(k == 0)
                 if(k == 0){
                     list.add(nums1[p]);
                     k++;
                     p++;
                     q++;
-                    
                 }else if(list.get(k-1) == nums1[p]){
-                    
                     p++;
                     q++;
-                    
                 }else{
                     list.add(nums1[p]);
                     k++;
                     p++;
                     q++;
                 }
-                    
             }else if(nums1[p] < nums2[q]){
                 p++;
             }else{
@@ -36,27 +31,12 @@ public class Solution {
         }
         
         int [] res = new int[k];
-        //if(k == 0) return res;
-        //else{
-          //  res[0] = list.get(0);
-            //k = k-1;
             int m = 0;
-            //int n = 1;
             while(k>0){
-                //if( list.get(m) == res[n-1]){
-                  //  m++;
-                    //k--;
-                //}else{
-                    res[m] = list.get(m);
-                    //n++;
-                    m++;
-                    k--;
-                //}
-            
+                res[m] = list.get(m);
+                m++;
+                k--;
             }
         return res;
-        //}
-        
-        
     }
 }
