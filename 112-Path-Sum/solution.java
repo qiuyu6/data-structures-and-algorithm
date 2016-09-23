a46bd5a1709282609701f55dbbc4bce13817bed9
qiuyu6 <qiuyu_93@hotmail.com>
2016-09-23 18:17:10 -0400

    [112. Path Sum][Accepted]committed by Qiu

--- 112-Path-Sum/solution.java
+++ 112-Path-Sum/solution.java
@@ -10,7 +10,23 @@
 public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if(root == null) return false;
-        if(root.left == null && root.right == null && sum - root.val == 0) return true;
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            TreeNode temp = stack.pop();
+            if(temp.left == null && temp.right == null && temp.val == sum) return true;
+            if(temp.left != null){
+                temp.left.val = temp.left.val + temp.val;
+                stack.push(temp.left);
+            }
+            if(temp.right != null){
+                temp.right.val = temp.right.val + temp.val;
+                stack.push(temp.right);
+            }
+        }
+        return false;
     }
 }
