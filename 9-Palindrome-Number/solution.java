ea1cd199482bda5bcc320d29d516f9edd4bb838e
qiuyu6 <qiuyu_93@hotmail.com>
2016-09-23 22:02:42 -0400

    [9. Palindrome Number][Accepted]committed by Qiu

--- 9-Palindrome-Number/solution.java
+++ 9-Palindrome-Number/solution.java
@@ -1,15 +1,11 @@
 public class Solution {
     public boolean isPalindrome(int x) {
-        if(x >= 0 && x <= 9) return true;
-        if(x < 0 || x%10 == 0) return false;
-        int sec = 0;
-        
-        while(x > sec){
-            int temp = x % 10;
-            sec = sec * 10 + temp;
-            x = x / 10;
+        int origin = x;
+        int compare = 0;
+        while(x > 0){
+            compare = compare * 10 + x % 10;
+            x = x/10;
         }
-        if(x == sec || sec/10 == x) return true;
-        return false;
+        return (origin == compare);
     }
 }
