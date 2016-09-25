6480e92f14c61af916ddfe947dc4e50bc0cbb094
qiuyu6 <qiuyu_93@hotmail.com>
2016-09-25 19:13:31 -0400

    [160. Intersection of Two Linked Lists][Accepted]committed by Qiu

--- 160-Intersection-of-Two-Linked-Lists/solution.java
+++ 160-Intersection-of-Two-Linked-Lists/solution.java
@@ -11,48 +11,15 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA == null || headB == null) return null;
-        int diff = 0;
-        ListNode first = headA;
-        ListNode second = headB;
-        boolean firstLong = true;
-        while(first.next != null && second.next != null){
-            first = first.next;
-            second = second.next;
-        }
-        if(first.next != null){
-            while(first.next != null){
-                diff++;
-                first = first.next;
-            }
-        }
-        if(second.next != null){
-            while(second.next != null){
-                diff++;
-                second = second.next;
-            }
-            firstLong = false;
-        }
-        if(firstLong){
-            first = headA;
-            second = headB;
-        }else{
-            second = headA;
-            first = headB;
-        }
-        while(diff>0){
-            first = first.next;
-            diff--;
-        }
-        while(first != null){
-            if(first == second) return first;
-            else{
-                first = first.next;
-                second = second.next;
-            }
-        }
-            
-        return null;
+        if( null==headA || null==headB )
+			return null;
+		
+		ListNode curA = headA, curB = headB;
+		while( curA!=curB){
+			curA = curA==null?headB:curA.next;
+			curB = curB==null?headA:curB.next;
+		}
+		return curA;
         
     }
 }
