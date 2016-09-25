5ea8366823f296b568d6d3a303f1ebc3e1cc9706
qiuyu6 <qiuyu_93@hotmail.com>
2016-09-25 10:19:25 -0400

    [20. Valid Parentheses][Accepted]committed by Qiu

---
+++ 20-Valid-Parentheses/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push(')');
+            }else if(s.charAt(i) == '{'){
+                stack.push('}');
+            }else if(s.charAt(i) == '['){
+                stack.push(']');
+            }else{
+                if(stack.isEmpty() || s.charAt(i) != stack.pop()) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
