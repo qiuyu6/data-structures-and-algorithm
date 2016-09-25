d14a488aea7e0e235214d10bce14f31362651c8e
qiuyu6 <qiuyu_93@hotmail.com>
2016-09-25 11:18:23 -0400

    [257. Binary Tree Paths][Accepted]committed by Qiu

---
+++ 257-Binary-Tree-Paths/solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<String>();
+        DFS(root, "", res);
+        return res;
+    }
+    
+    public void DFS(TreeNode root, String newPath, List wholePath){
+        if(root == null) return;
+        if(root.left == null && root.right == null) wholePath.add(newPath + root.val);
+        if(root.left != null)  DFS(root.left, newPath + root.val + "->", wholePath);
+        if(root.right != null)  DFS(root.right, newPath + root.val + "->", wholePath);
+    }
+}
